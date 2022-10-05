3d43613cea82acd544c3540817780743f4ff5314
AnnaLinden <linden.a.anna@gmail.com>
2022-10-05 16:25:26 +0300

    created the branch for anna

--- current_time.py
+++ current_time.py
@@ -1,2 +1,16 @@
 # 1) calculate the current time of the location 
 # - use longitude, latitude, and time API
+import requests
+import json
+
+airport_latitude = 0
+airport_longitude = 0
+
+response = requests.get(f'https://timeapi.io/api/Time/current/coordinate?latitude={airport_latitude}&longitude={airport_longitude}').json()
+#print(response)
+
+
+init_time = (response["time"])
+print(f"The time in the befinning of the game is: {init_time}")
+
+
