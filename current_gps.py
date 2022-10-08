b4822f48387a284102a0129a042a356c4e3a44f3
Chaeah Park <102865440+chepark@users.noreply.github.com>
2022-10-08 20:57:19 +0300

    Organized the code.

--- current_gps.py
+++ current_gps.py
@@ -1,35 +1,32 @@
-# 1) get the longitude and latitude of the current airport 
-# - use DB
-
 import mysql.connector
 import _creds
+import user_data
 
-airport_latitude = 0
-airport_longitude = 0
-# get latitude and longitude from player's current airport and chosen airport
 def get_chosen_airport_gps(airport_name):
     sql = "SELECT name, latitude_deg, longitude_deg FROM Airport"
     sql += " WHERE name ='" + airport_name + "'"
-    global airport_latitude
-    global airport_longitude
+
     cursor = connection.cursor()
     cursor.execute(sql)
     result = cursor.fetchall()
+
     if cursor.rowcount > 0:
         for row in result:
-            # Limit floats to two decimals
             latitude =  round(row[1], 2)
             longitude = round(row[2], 2)
-            print(f"This is {row[0]} airport, which has latitude is {latitude} and longitude is {longitude}.")
+
+            if not user_data.new_location:
+                print(f"This is {row[0]} airport, which has latitude is {latitude} and longitude is {longitude}. \n")
             
+            global airport_latitude
+            global airport_longitude
+
             airport_latitude = row[1]
             airport_longitude = row[2]
+          
     return [airport_latitude, airport_longitude]
 
-
-# connect to DB
-# !! password value is changed to "_creds.db_password"
-# !! database value is changed to "_creads.db_name". 
+# CONNECT TO DB
 connection = mysql.connector.connect(
          host='127.0.0.1',
          port= 3306,
@@ -38,14 +35,3 @@ connection = mysql.connector.connect(
          password=_creds.db_password,
          autocommit=True
          )
-# Player enter airport name to get data of latitude and longitude
-
-"""
-current_airport = input("Enter airport name: ")
-current_location = get_chosen_airport_gps(current_airport)
-print(current_location)
-
-chosen_airport = input("Enter the name of airport that you want to go from the list: ")
-new_location = get_chosen_airport_gps(chosen_airport)
-print(new_location)
-"""
