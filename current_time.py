b8acf64451901d42c22dc27c5601f7afcf2e0c15
Chaeah Park <102865440+chepark@users.noreply.github.com>
2022-10-08 16:34:41 +0300

    Changed the data type of returning value. Modified the codes.

--- current_time.py
+++ current_time.py
@@ -1,22 +1,15 @@
-# 1) calculate the current time of the location 
-# - use longitude, latitude, and time API
+# CALCULATE THE CURRENT TIME OF THE LOCATION
+
 import requests
 import json
 from datetime import datetime
-import current_gps
 
-airport_latitude = 0
-airport_longitude = 0
-
-def get_init_time():
+def get_airport_time(airport_latitude, airport_longitude):
     response = requests.get(f'https://timeapi.io/api/Time/current/coordinate?latitude={airport_latitude}&longitude={airport_longitude}').json()
     init_hour = (response["hour"])
     init_min = (response["minute"])
     init_time = datetime.strptime(f"{init_hour}:{init_min}:00", "%H:%M:%S")
     init_time = init_time.time().strftime("%H:%M")
-    print(f"The time in the beginning of the game is: {init_time}")
-    return init_time
+    print(f"The time of the current airport is {init_time}")
+    return {'time': init_time, 'hour': init_hour, 'min': init_min}
 
-response = requests.get(f'https://timeapi.io/api/Time/current/coordinate?latitude={airport_latitude}&longitude={airport_longitude}').json()
-init_hour = (response["hour"])
-init_min = (response["minute"])
